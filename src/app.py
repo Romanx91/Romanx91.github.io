96b2eaaa3479024af3d66e2710d7ab94f3b71632
Romanx91 <romanpy7@gmail.com>
2023-08-23 19:09:54 -0300

    trabajo terminado

--- src/app.py
+++ src/app.py
@@ -8,8 +8,33 @@ app.static_folder = 'static'
 
 
 @app.route('/')
-def prueba():
-    return render_template('auth/prueba.html')
+def home():
+    return render_template('auth/home.html')
+
+
+@app.route('/faqs')
+def faqs():
+    return render_template('auth/faqs.html')
+
+
+@app.route('/funds')
+def funds():
+    return render_template('auth/funds.html')
+
+
+@app.route('/thefirm')
+def thefirm():
+    return render_template('auth/thefirm.html')
+
+
+@app.route('/team')
+def team():
+    return render_template('auth/team.html')
+
+
+@app.route('/invest')
+def invest():
+    return render_template('auth/invest.html')
 
 
 if __name__ == '__main__':
--- src/static/js/script.js
+++ src/static/js/script.js
@@ -3,6 +3,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const loginForm = document.querySelector(".loging");
   const closeButton = document.querySelector(".back");
 
+  const createaccountButton = document.querySelector(".ct");
+  const createaccountForm = document.querySelector(".createaccount");
+  const CloseButton = document.querySelector(".Back");
+
+  const cuentacreadaButton = document.querySelector(".buttSub");
+  const cuentacreadaForm = document.querySelector(".cuentacreada");
+  const ClosecuentacreadaButton = document.querySelector(".ok");
+
   if (loginButton && loginForm) {
     loginButton.addEventListener("click", function () {
       loginForm.style.display =
@@ -16,6 +24,38 @@ document.addEventListener("DOMContentLoaded", function () {
     loginForm.addEventListener("submit", function (event) {
       event.preventDefault();
       loginForm.style.display = "none";
+    });
+  }
+
+  if (createaccountButton && createaccountForm) {
+    createaccountButton.addEventListener("click", function () {
+      createaccountForm.style.display =
+        createaccountForm.style.display === "block" ? "none" : "block";
+    });
+
+    CloseButton.addEventListener("click", function () {
+      createaccountForm.style.display = "none";
+    });
+
+    createaccountForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+      createaccountForm.style.display = "none";
+    });
+  }
+
+  if (cuentacreadaButton && cuentacreadaForm) {
+    cuentacreadaButton.addEventListener("click", function () {
+      cuentacreadaForm.style.display =
+        cuentacreadaForm.style.display === "block" ? "none" : "block";
+    });
+
+    ClosecuentacreadaButton.addEventListener("click", function () {
+      cuentacreadaForm.style.display = "none";
+    });
+
+    cuentacreadaForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+      cuentacreadaForm.style.display = "none";
     });
   }
 });
